34edeeb1b9baf8d441f7a7c33a04292c49f8cfb2
r-ejilemele <r.ejilemele@gmail.com>
2024-05-26 20:14:19 -0500

    Added Navbar/Sidebar and added basic routing through the Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+"use client";
+import React, { useState } from "react";
+import { VscThreeBars } from "react-icons/vsc";
+import Sidebar from "./Sidebar";
+import SidebarCSS from "./Sidebar.css"
+import Link from 'next/link'
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className=" flex-col">
+      <span className="hidden md:flex rounded-b-xl bg-black font-sans h-[10vh] items-center px-24 lg:px-36 justify-between text-gray-200 fixed top-0 w-full z-50 bg-opacity-85">
+        <Link className="text-2xl font-bold text-white" href="/">TECHNIQUE</Link>
+        <div className="flex space-x-4">
+          <Link className="hover:text-white" href="/">HOME</Link>
+          <Link className="hover:text-white" href="/about">ABOUT</Link>
+          <Link className="hover:text-white" href="/seniors">SENIORS</Link>
+          <Link className="hover:text-white" href="/hire">HIRE US</Link>
+          <Link className="hover:text-white" href="/join">JOIN US</Link>
+        </div>
+      </span>
+      <div className="bg-black  h-[10vh] rounded-b-xl md:hidden flex fixed top-0 z-30 w-full items-center font-sans justify-between px-4 bg-opacity-85">
+        <Link className="text-xl font-bold text-white" href="/">TECHNIQUE</Link>
+        <VscThreeBars
+          style={{ color: "white" }}
+          onClick={() => setIsOpen(true)}
+        />
+        {isOpen ? <Sidebar setIsOpen={setIsOpen}></Sidebar> : null}
+
+        {/* <div className="bg-blue-300 w-[70vw] h-[100vh] absolute right-0 top-0">
+          <p className={`text-black ${isOpen ? "flex": "hidden"}`}> CLOSE P2</p>
+        </div> */}
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,64 @@
+"use client";
+import React, { useState } from "react";
+import { IoCloseCircleOutline } from "react-icons/io5";
+import SideBar from "./Sidebar.css";
+import Link from 'next/link'
+
+
+function Sidebar(props) {
+  const [close, setClose] = useState(false);
+  const targetElement = document.getElementById('element100');
+//   targetElement.focus()
+// console.log(targetElement)
+// targetElement?.focus()
+// console.log(targetElement)
+
+  return (
+    <div
+      className={`w-[100vw] h-[100vh] bg-black absolute top-0 right-0 pointer-events-auto overflow-none ${
+        !close ? "bg-black bg-opacity-15" : "bg-opacity-0"
+      }`}
+      id="element100"
+    >
+      <div
+        className={`bg-red-800 w-[70vw] h-[100vh] absolute right-0 top-0 rounded-l-3xl flex-col pullLeft ${
+          close ? "pullRight" : ""
+        }`}
+      >
+        <span className="w-[100%] h-16 flex justify-end p-6">
+          <IoCloseCircleOutline
+            style={{ height: "5vh", width: "5vh", color: "white" }}
+            onClick={() => {
+              setClose(true);
+              setTimeout(function () {
+                props.setIsOpen(false);
+                setClose(false);
+              }, 400);
+            }}
+          />
+        </span>
+        <ul className="space-y-4 ">
+          <Link className="pl-6 w-full  h-[7vh] text-white flex items-center hover:bg-red-900 active:bg-red-700" href="/">
+            HOME
+          </Link>
+          <Link className="w-full pl-6 h-[7vh] text-white flex items-center hover:bg-red-900 active:bg-red-700" href="/about">
+            ABOUT
+          </Link>
+          <Link className="w-full pl-6 h-[7vh] text-white flex items-center hover:bg-red-900 active:bg-red-700" href="/seniors">
+            SENIORS
+          </Link>
+          <Link className="w-full pl-6 h-[7vh] text-white flex items-center hover:bg-red-900 active:bg-red-700" href="/hire">
+            HIRE US
+          </Link>
+          <Link className="w-full pl-6 h-[7vh] text-white flex items-center hover:bg-red-900 active:bg-red-700" href="/join">
+            JOIN US
+          </Link>
+        </ul>
+      </div>
+      
+    </div>
+    
+  );
+}
+
+export default Sidebar;
