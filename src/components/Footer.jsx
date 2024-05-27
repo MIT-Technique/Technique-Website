2a38dc1d335ab8ed5ae6017e5c5ae34ee28fe4c1
r-ejilemele <r.ejilemele@gmail.com>
2024-05-27 16:30:40 -0500

    changing some meta-data

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='bg-black h-[15vh] w-full'>
+    <div className='bg-black h-[15vh] w-full z-50 relative'>
 
     </div>
   )
