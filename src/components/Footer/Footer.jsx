3e75b5a81c9ce449a117102e552f2085ac860671
r-ejilemele <r.ejilemele@gmail.com>
2024-07-16 21:42:38 -0400

    ignore eslint for deployment

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
     <div className={`bg-black h-[10vh] w-full z-50 relative text-gray-200 p-12 lg:flex items-center justify-between text-xs ${/*"bg-opacity-85"*/""}`}>
       <div>&copy; 2024 Technique. Images are property of their respective owners. All Rights Reserved.</div>
       <div className='flex lg:flex-row items-center justify-end'>
-        <a href='mailto:tnq-exec@mit.edu'>
+        <a href='mailto:technique@mit.edu'>
           <Tooltip title="Email Us" arrow>
             <MailIcon/>
           </Tooltip>
