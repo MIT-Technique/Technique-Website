5f90605058f34527d2a35e470fa30ce1c274bdd6
r-ejilemele <r.ejilemele@gmail.com>
2024-07-20 18:21:08 -0400

    Fixed contact page image quality

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -16,14 +16,13 @@ function page() {
               We would love to hear from you!
             </p>
             <p className=" text-center text-black font-[250]">
-              If you need photography for an event, our community of MIT
-              photographers can help.
+              Feel free to send us a message or find us in the office.
             </p>
           </div>
           <div className="relative w-full flex justify-between ">
-            <div className=" flex flex-col items-center w-1/2">
-              <div className="flex flex-col space-y-2 items-center">
-                <h className=" text-xl"> WANT TO GET IN TOUCH?</h>
+            <div className=" flex flex-col items-center w-1/2 pt-5 space-y-3">
+              <div className="flex flex-col space-y-1 items-center">
+                <h className=" text-lg font-[400]"> WANT TO GET IN TOUCH?</h>
                 <Link
                   href="mailto:technique@mit.edu"
                   className="text-blue-400 font-[300]"
@@ -31,12 +30,20 @@ function page() {
                   Email us!
                 </Link>
               </div>
-              <br></br>
-              <div className="flex flex-col items-center  space-y-2 text-gray-500">
-                <h className=" text-xl "> VISIT US IN PERSON</h>
-                <div className="flex flex-col items-center font-[250]">
+              <div className="flex flex-col items-center  space-y-1 text-gray-500">
+                <h className=" text-base "> VISIT US IN PERSON</h>
+                <div className="flex flex-col items-center font-[250] text-sm">
                   <p>142 Memorial Dr.</p>
                   <p>Walker Memorial</p>
+                  <p>50-320</p>
+                </div>
+              </div>
+              <div className="flex flex-col items-center  space-y-1 text-gray-500">
+                <h className=" text-base "> MAILING ADDRESS</h>
+                <div className="flex flex-col items-center font-[250] text-sm">
+                  <p>MIT Technique</p>
+                  <p>32 Vassar Street</p>
+                  <p>Cambridge, Ma, 02139</p>
                   <p>50-320</p>
                 </div>
               </div>
@@ -45,8 +52,8 @@ function page() {
               <Image
                 src="/images/other_images/Jade_Chongsathapornpong/DSC02130.jpg"
                 alt=""
-                width={600}
-                height={600}
+                width={2000}
+                height={2000}
                 priority
               />
               <p
