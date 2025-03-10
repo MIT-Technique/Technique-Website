3e00844c4ab5ad720ade29845ac9a8ffcfc84e84
Ruhundaka E. <r.ejilemele@gmail.com>
2025-03-09 21:42:08 -0400

    Added a redirect to the preorder site so people can go to technique.mit.edu/preorder

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/preorder",
+        destination: "https://mit.universitytickets.com/w/event.aspx?id=2127",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
