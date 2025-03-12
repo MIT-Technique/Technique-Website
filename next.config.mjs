9f7ec6e3442f796b8ae4a016ae9965c358451af0
Ruhundaka E. <r.ejilemele@gmail.com>
2025-03-12 12:42:08 -0400

    added redirection to the senior portrait submission

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,11 @@ const nextConfig = {
         destination: "https://mit.universitytickets.com/w/event.aspx?id=2127",
         permanent: false,
       },
+      {
+        source: "/submission",
+        destination: "https://forms.gle/D6bw7LksKv94Y3Pd8",
+        permanent: false,
+      },
     ];
   },
 };
