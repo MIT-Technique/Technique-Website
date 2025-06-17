a77b59e4f6d3088bfe432e930f650bad4d412cdb
Ruhundaka E. <r.ejilemele@gmail.com>
2025-06-17 18:19:30 -0400

    debugging3

--- src/app/api/sendInvoice/route.ts
+++ src/app/api/sendInvoice/route.ts
@@ -19,7 +19,9 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
   const FROM_EMAIL: string = "mittnq@gmail.com";
 
   try {
-    console.log(`Files: ${await fs.promises.readdir(process.cwd())}`);
+    console.log(
+      `Files: ${await fs.promises.readdir(path.join(process.cwd(), "public"))}`
+    );
     const filePath = path.join(
       process.cwd(),
       "public",
