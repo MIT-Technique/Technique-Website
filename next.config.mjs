26e746094b324790e9914bab3753da18a5cd397f
Ruhundaka E. <r.ejilemele@gmail.com>
2026-01-17 17:43:31 -0500

    Adding purchase link to our domain

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,11 @@ const nextConfig = {
           "https://seniors.legacystudios.com/massachusetts-institute-technology-cambridge-ma/",
         permanent: false,
       },
+      {
+        source: "/purchase",
+        destination: "https://engage.mit.edu/technique/rsvp_boot?id=916938",
+        permanent: false,
+      },
     ];
   },
 };
