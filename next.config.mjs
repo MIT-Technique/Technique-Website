ef86e395de89c27403f8fe386bea34b827c82749
Max-zhuuu <mzabcde@gmail.com>
2026-01-17 21:43:29 -0500

    Link FIx

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,11 @@ const nextConfig = {
           "https://seniors.legacystudios.com/massachusetts-institute-technology-cambridge-ma/",
         permanent: false,
       },
+      {
+        source: "/:locale/purchase",
+        destination: "https://engage.mit.edu/technique/rsvp_boot?id=916938",
+        permanent: false,
+      },
     ];
   },
 };
