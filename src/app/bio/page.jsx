4fb52323d8bcaad4d79e09bff02a6ffcd1b81a7e
Ruhundaka E. <r.ejilemele@gmail.com>
2026-01-19 15:03:28 -0500

    Fixing small bug

--- src/app/bio/page.jsx
+++ src/app/bio/page.jsx
@@ -66,6 +66,7 @@ export default function BioPage() {
           setSecondMajor(json.data.second_major);
         }
         setQuote(json.data.quote);
+        setExtracurriculars(json.data.achievements);
       } catch (err) {
         // setError(err.message);
       } finally {
